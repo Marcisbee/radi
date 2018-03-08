517a507ec613003b365aefc9f4fe44b94f71d1e3
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-03-08 11:33:40 +0100

    Added radiMutate tests

---
+++ src/utilities/__tests__/radiMutate.test.js
@@ -0,0 +1,10 @@
+import sinon from "sinon";
+import { radiMutate } from '../radiMutate';
+
+describe("radiMutate.js", () => {
+  it("calls the first parameter", () => {
+    const fnSpy = sinon.spy();
+    radiMutate(fnSpy);
+    expect(fnSpy.calledOnce).toBe(true);
+  });
+});
