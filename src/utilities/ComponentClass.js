c4cdb3a86301daf97ad45ef7d57dfbc137c55886
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-03-08 17:33:39 +0100

    Refactored ComponentClass.js

--- src/utilities/ComponentClass.js
+++ src/utilities/ComponentClass.js
@@ -2,36 +2,36 @@ import { Radi } from "../index";
 import { GLOBALS } from "../consts/GLOBALS";
 import { clone } from "./clone";
 
-function Component(o) {
-  this.o = {
-    name: o.name,
-    state: clone(o.state),
-    props: clone(o.props),
-    actions: o.actions,
-    view: o.view,
-    $view: o.$view,
-    $mixins: this.$mixins || {}
-  };
+export class Component {
+  constructor(o) {
+    this.o = {
+      name: o.name,
+      state: clone(o.state),
+      props: clone(o.props),
+      actions: o.actions,
+      view: o.view,
+      $view: o.$view,
+      $mixins: this.$mixins || {},
+    };
 
-  this.__radi = function() {
-    return new Radi(this.o);
-  };
-}
-
-Component.prototype.props = function props(p) {
-  for (var k in p) {
-    if (typeof this.o.props[k] === "undefined") {
-      console.warn(
-        "[Radi.js] Warn: Creating a prop `",
-        k,
-        "` that is not defined in component"
-      );
-    }
-    this.o.props[k] = p[k];
+    this.$mixins = {};
   }
-  return this;
-};
 
-Component.prototype.$mixins = {};
+  __radi() {
+    return new Radi(this.o);
+  }
 
-export { Component };
+  props(propsUpdates) {
+    for (let key in propsUpdates) {
+      if (typeof this.o.props[key] === "undefined") {
+        console.warn(
+          "[Radi.js] Warn: Creating a prop `",
+          key,
+          "` that is not defined in component"
+        );
+      }
+      this.o.props[key] = propsUpdates[key];
+    }
+    return this;
+  }
+}
