3afeeb3351818cd15ad90137872c00442121fe8c
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-03-08 18:33:21 +0100

    Refactored r.js

--- src/utilities/r.js
+++ src/utilities/r.js
@@ -4,44 +4,38 @@ import { isNode } from "../index";
 import { radiArgs } from "../index";
 import { GLOBALS } from "../consts/GLOBALS";
 
-export function r(query) {
-  // TODO: just use .slice(1)
-  var args = [],
-    len = arguments.length - 1;
-  while (len-- > 0) args[len] = arguments[len + 1];
-
-  var element;
-
-  if (isString(query)) {
-    if (typeof GLOBALS.REGISTERED[query] !== "undefined") {
-      // TODO: Make props and childs looped,
-      // aka don't assume that first obj are props
-      var props = args[0] || {};
-      return (element = new GLOBALS.REGISTERED[query]().props(props));
-    } else {
-      element = memoizeHTML(query).cloneNode(false);
-    }
-  } else if (isNode(query)) {
-    element = query.cloneNode(false);
-  } else {
-    element = document.createDocumentFragment();
+export const r = (query, ...args) => {
+  if (queryIsComponent(query)) {
+    // TODO: Make props and childs looped,
+    // aka don't assume that first obj are props
+    const props = args[0] || {};
+    return new GLOBALS.REGISTERED[query]().props(props);
   }
 
-  element.key = GLOBALS.R_KEYS;
-  GLOBALS.R_KEYS += 1;
+  const element = getElementFromQuery(query);
+  addKey(element);
 
   radiArgs.call(this, element, args);
 
   return element;
 }
 
-r.extend = function(query) {
-  // TODO: just use .slice(1)
-  var args = [],
-    len = arguments.length - 1;
-  while (len-- > 0) args[len] = arguments[len + 1];
+export const queryIsComponent = (query) =>
+  isString(query) && typeof GLOBALS.REGISTERED[query] !== "undefined";
 
-  var clone = memoizeHTML(query);
+export const getElementFromQuery = (query) => {
+  if (isString(query)) return memoizeHTML(query).cloneNode(false);
+  if (isNode(query)) return query.cloneNode(false);
+  return document.createDocumentFragment();
+};
 
+export const addKey = (element) => {
+  element.key = GLOBALS.R_KEYS;
+  GLOBALS.R_KEYS += 1;
+  return element;
+};
+
+r.extend = (query, ...args) => {
+  const clone = memoizeHTML(query);
   return r.bind.apply(r, [this, clone].concat(args));
 };
