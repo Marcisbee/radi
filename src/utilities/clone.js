7bd48630bffedd88177521c4cdd676f42321a286
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-03-08 17:16:16 +0100

    Refactored clone.js

--- src/utilities/clone.js
+++ src/utilities/clone.js
@@ -1,32 +1,14 @@
 export const clone = obj => {
-  let i, ret, ret2;
-  if (typeof obj === "object") {
-    if (obj === null) return obj;
-    if (Object.prototype.toString.call(obj) === "[object Array]") {
-      let len = obj.length;
-      ret = new Array(len);
-      for (i = 0; i < len; i++) {
-        if (typeof obj[i] === "object") {
-          ret[i] = clone(obj[i]);
-        } else {
-          ret[i] = obj[i];
-        }
-      }
-    } else {
-      ret = {};
-      for (i in obj) {
-        if (obj.hasOwnProperty(i)) {
-          if (typeof (obj[i] === "object")) {
-            ret[i] = clone(obj[i]);
-          } else {
-            ret[i] = obj[i];
-          }
-        }
-      }
+  if (typeof obj !== "object") return obj;
+  if (obj === null) return obj;
+  if (Array.isArray(obj)) return obj.map(element => clone(element));
+
+  const cloned = {};
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      cloned[key] = clone(obj[key]);
     }
-  } else {
-    ret = obj;
   }
 
-  return ret;
+  return cloned;
 };
