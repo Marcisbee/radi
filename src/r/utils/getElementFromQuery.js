ddda826fd93021fff4ce13fa8ab903ea6fb2fac5
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-03-12 21:42:25 +0100

    Removed HTML caching because cached elements were cloned anyways

--- src/r/utils/getElementFromQuery.js
+++ src/r/utils/getElementFromQuery.js
@@ -1,11 +1,9 @@
-import cacheHtml from './cacheHtml';
-
 /**
  * @param {*} query
  * @returns {Node}
  */
 const getElementFromQuery = (query) => {
-  if (typeof query === 'string') return cacheHtml(query).cloneNode(false);
+  if (typeof query === 'string') return document.createElement(query);
   if (isNode(query)) return query.cloneNode(false);
   return document.createDocumentFragment();
 };
