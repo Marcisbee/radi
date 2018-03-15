679efae58d5d86051487aa0e5c861897eccecb91
Marcisbee <marcisbergmanis@gmail.com>
2018-03-15 23:51:03 +0200

    Adds individual StyleListener

---
+++ src/r/utils/StyleListener.js
@@ -0,0 +1,44 @@
+import setStyle from './setStyle';
+
+export default class StyleListener {
+  /**
+   * @param {object} options
+   * @param {string} options.styleKey
+   * @param {Listener} options.listener
+   * @param {Node} options.element
+   */
+  constructor({ styleKey, listener, element }) {
+    this.styleKey = styleKey;
+    this.listener = listener;
+    this.element = element;
+    this.attached = false;
+    this.handleValueChange = this.handleValueChange.bind(this);
+  }
+
+  /**
+   * Attaches style listener to given element and starts listening.
+   * @returns {StyleListener}
+   */
+  attach() {
+    if (!this.element.styleListeners) this.element.styleListeners = [];
+    this.element.styleListeners.push(this);
+    this.listener.onValueChange(this.handleValueChange);
+    this.attached = true;
+    return this;
+  }
+
+  /**
+   * @param {*} value
+   */
+  handleValueChange(value) {
+    setStyle(this.element, this.styleKey, value);
+  }
+
+  /**
+   * @param {Node} newElement
+   */
+  updateElement(newElement) {
+    this.element = newElement;
+    return this.element;
+  }
+}
--- src/r/utils/setStyle.js
+++ src/r/utils/setStyle.js
@@ -1,3 +1,5 @@
+import Listener from '../../listen/Listener';
+import StyleListener from '../utils/StyleListener';
 import parseValue from './parseValue';
 
 /**
@@ -8,6 +10,16 @@ import parseValue from './parseValue';
  */
 const setStyle = (element, property, value) => {
   if (typeof value === 'undefined') return;
+
+  if (value instanceof Listener) {
+    new StyleListener({
+      styleKey: property,
+      listener: value,
+      element,
+    }).attach();
+    return element[property];
+  }
+
   return element.style[property] = parseValue(value);
 };
 
