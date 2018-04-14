f27219dfc96744caa11101e3f868a74b01457425
Marcisbee <marcisbergmanis@gmail.com>
2018-04-14 16:08:02 +0300

    Allow to use <template> tag

--- src/r/utils/getElementFromQuery.js
+++ src/r/utils/getElementFromQuery.js
@@ -3,7 +3,9 @@
  * @returns {Node}
  */
 const getElementFromQuery = query => {
-  if (typeof query === 'string') return document.createElement(query);
+  if (typeof query === 'string') return query !== 'template'
+    ? document.createElement(query)
+    : document.createDocumentFragment();
   console.warn(
     '[Radi.js] Warn: Creating a JSX element whose query is not of type string, automatically converting query to string.'
   );
