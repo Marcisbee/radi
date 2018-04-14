584023e767db86cc3cfc303d8ac0fe9ccab0682f
Marcisbee <marcisbergmanis@gmail.com>
2018-04-14 16:05:45 +0300

    Allows mount to have array passed

--- src/mount.js
+++ src/mount.js
@@ -6,11 +6,26 @@ import Component from './component/Component';
  * @returns {HTMLElement|Node}
  */
 const mount = (component, id) => {
-  const container = typeof id === 'string' ? document.getElementById(id) : id;
+  const container = document.createDocumentFragment()
+  const slot = typeof id === 'string' ? document.getElementById(id) : id;
   const rendered =
-    component instanceof Component ? component.render() : component;
-  container.appendChild(rendered);
+    (component instanceof Component || component.render) ? component.render() : component;
+
+  if (Array.isArray(rendered)) {
+    for (var i = 0; i < rendered.length; i++) {
+      mount(rendered[i], container)
+    }
+  } else {
+    // Mount to container
+    container.appendChild(rendered);
+  }
+
+  // Mount to element
+  slot.appendChild(container);
+
+  if (typeof component.mount === 'function') component.mount();
+
   return rendered;
-};
+}
 
 export default mount;
