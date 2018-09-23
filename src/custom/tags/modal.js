f8b92ce94544ec1ea097c9281619aebe790e4d0f
Marcisbee <marcisbergmanis@gmail.com>
2018-09-23 10:21:15 +0300

    Minor code cleanup

--- src/custom/tags/modal.js
+++ src/custom/tags/modal.js
@@ -18,6 +18,10 @@ export const ModalService = service('modal', (...args) => {
   return {
     open: (name) => ModalStore.dispatch(switchModal, name, true),
     close: (name) => ModalStore.dispatch(switchModal, name, false),
+    onOpen: (name, fn) =>
+      ModalStore.subscribe((n, p) => n[name] === true && n[name] !== p[name] && fn()),
+    onClose: (name, fn) =>
+      ModalStore.subscribe((n, p) => n[name] === false && n[name] !== p[name] && fn()),
   };
 });
 
@@ -27,39 +31,6 @@ customTag('modal',
       console.warn('[Radi.js] Warn: Every <modal> tag needs to have `name` attribute!');
     }
 
-    this.style = `
-      .--radi-modal {
-        display: block;
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 1000;
-      }
-      .--radi-modal-backdrop {
-        display: block;
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 0;
-        background-color: rgba(0, 0, 0, 0.7);
-      }
-      .--radi-modal-content {
-        display: block;
-        position: relative;
-        width: 600px;
-        max-width: 98%;
-        z-index: 1;
-        margin: 40px auto;
-        border-radius: 8px;
-        box-shadow: 0 5px 0px rgba(0, 0, 0, 0.1), 0 20px 50px rgba(0, 0, 0, 0.3);
-        background-color: #fff;
-        padding: 26px;
-      }
-    `
     this.onMount = el => ModalStore.dispatch(registerModal, name);
 
     return h('portal', {},
