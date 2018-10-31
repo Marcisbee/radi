c4445cdcaca7da2d9f7736f7d5c81a34eff4e9d2
Marcisbee <marcisbergmanis@gmail.com>
2018-10-31 23:50:01 +0200

    Rendering cache

--- dist/radi.es.js
+++ dist/radi.es.js
@@ -792,87 +792,6 @@ function render$$1(node, $parent) {
   return element;
 }
 
-// import { fireEvent } from './fireEvent';
-// import { nodeChanged } from './nodeChanged';
-// import { updateProps } from './props';
-
-
-/**
- * @param  {Object|Object[]} nodes
- * @param  {HTMLElement} dom
- * @param  {HTMLElement} parent
- * @param  {HTMLElement} $pointer
- * @returns {HTMLElement|HTMLElement[]}
- */
-function patch(nodes, dom, parent, $pointer) {
-  if ( parent === void 0 ) parent = dom && dom.parentNode;
-  if ( $pointer === void 0 ) $pointer = null;
-
-  if (Array.isArray(nodes) || Array.isArray(dom)) {
-    var flatNodes = Array.isArray(nodes) && nodes.length === 0 ? [null] : ensureArray(nodes);
-    var flatDom = ensureArray(dom);
-    var ref = Array.isArray(dom)
-      ? dom[0] && dom[0].__radiRef
-      : dom.__radiRef;
-
-    var lastNode = dom;
-
-    var outcome = flatNodes.map(function (node, ii) {
-      var staticLastNode = lastNode;
-      var outputNode = patch(
-        node,
-        flatDom[ii],
-        (flatDom[ii] && flatDom[ii].parentNode) || parent,
-        !flatDom[ii] && staticLastNode
-      );
-
-      // Need to memoize last rendered node
-      lastNode = Array.isArray(outputNode)
-        ? outputNode[0]
-        : outputNode;
-
-      // Make nested & updated components update their refrences
-      if (typeof ref === 'function' && ii > 0) {
-        lastNode.__radiRef = function (data) { return ref(data, ii); };
-        lastNode.__radiRef(lastNode, ii);
-      }
-
-      return outputNode;
-    });
-
-    // Unused nodes can be savely remove from DOM
-    if (flatDom.length > flatNodes.length) {
-      var unusedDomNodes = flatDom.slice(flatNodes.length - flatDom.length);
-      unusedDomNodes.forEach(destroy);
-    }
-
-    // Pass new nodes refrence to function containing it
-    if (typeof ref === 'function') { ref(outcome); }
-
-    return outcome;
-  }
-
-  var newNode = render$$1(nodes, parent);
-
-  if (!dom && $pointer) {
-    var mounter = mount(newNode, dom, $pointer);
-    return mounter;
-  }
-
-  if (dom && parent) {
-    mount(newNode, parent, dom);
-
-    if (dom.__radiRef) {
-      newNode.__radiRef = dom.__radiRef;
-      newNode.__radiRef(newNode);
-    }
-
-    destroy(dom);
-  }
-
-  return newNode;
-}
-
 function autoUpdate(value, fn) {
   if (value instanceof Listener) {
     return fn(value.getValue(function (e) { return fn(value.map(e)); }));
@@ -976,6 +895,161 @@ function addEventListener($target, name, value) {
       false
     );
   }
+}
+
+/**
+ * @param  {Object|Object[]} nodes
+ * @param  {HTMLElement} dom
+ * @param  {HTMLElement} parent
+ * @param  {HTMLElement} $pointer
+ * @returns {HTMLElement|HTMLElement[]}
+ */
+function patch(nodes, dom, parent, $pointer) {
+  if ( parent === void 0 ) parent = dom && dom.parentNode;
+  if ( $pointer === void 0 ) $pointer = null;
+
+  if (Array.isArray(nodes) || Array.isArray(dom)) {
+    var flatNodes = Array.isArray(nodes) && nodes.length === 0 ? [null] : ensureArray(nodes);
+    var flatDom = ensureArray(dom);
+    var ref = Array.isArray(dom)
+      ? dom[0] && dom[0].__radiRef
+      : dom.__radiRef;
+
+    var lastNode = dom;
+
+    var outcome = flatNodes.map(function (node, ii) {
+      var staticLastNode = lastNode;
+      var outputNode = patch(
+        node,
+        flatDom[ii],
+        (flatDom[ii] && flatDom[ii].parentNode) || parent,
+        !flatDom[ii] && staticLastNode
+      );
+
+      // Need to memoize last rendered node
+      lastNode = Array.isArray(outputNode)
+        ? outputNode[0]
+        : outputNode;
+
+      // Make nested & updated components update their refrences
+      if (typeof ref === 'function' && ii > 0) {
+        lastNode.__radiRef = function (data) { return ref(data, ii); };
+        lastNode.__radiRef(lastNode, ii);
+      }
+
+      return outputNode;
+    });
+
+    // Unused nodes can be savely remove from DOM
+    if (flatDom.length > flatNodes.length) {
+      var unusedDomNodes = flatDom.slice(flatNodes.length - flatDom.length);
+      unusedDomNodes.forEach(destroy);
+    }
+
+    // Pass new nodes refrence to function containing it
+    if (typeof ref === 'function') { ref(outcome); }
+
+    return outcome;
+  }
+
+  /*
+    TODO: Make dom structure in before making HTMLElement
+    so that we can diff that structure with existing DOM
+
+    Preferrably it would look like this:
+
+    ```
+    {
+      type: 0,
+      query: function Component() {},
+      props: {},
+      children: [
+        {
+          type: 1,
+          query: 'button',
+          props: {
+            onclick: function dispatch() {}
+          },
+          children: [
+            {
+              type: 2,
+              query: 'Click me'
+            }
+          ]
+        }
+      ]
+    }
+    ```
+  */
+
+  /* Currently we'll render HTMLElements and diff them with real dom */
+
+  var newNode = render$$1(nodes, parent);
+
+  return patchDomRecursively(dom, newNode, parent, $pointer);
+}
+
+function nodeChanged$1(node1, node2) {
+  if (node1.nodeType === 3 && node2.nodeType === 3) { return true; }
+  if (node1.nodeType === node2.nodeType) { return false; }
+  if (node1.nodeName === node2.nodeName) { return false; }
+
+  return true;
+}
+
+function patchDomRecursively(oldDom, newDom, parent, pointer) {
+  if (!oldDom && (pointer || parent)) {
+    var mounter = mount(newDom, parent, pointer);
+    return mounter;
+  }
+
+  if (typeof newDom === 'undefined') {
+    destroy(oldDom);
+    return oldDom;
+  }
+
+  if (oldDom && parent) {
+    if (!nodeChanged$1(oldDom, newDom)) {
+
+      /* TODO: implement text change */
+      // if (oldDom.nodeType === 3 && newDom.nodeType === 3
+      //   && oldDom.textContent !== newDom.textContent) {
+      //   oldDom.textContent = newDom.textContent;
+      // }
+
+      if (oldDom.childNodes || newDom.childNodes) {
+        var length = Math.max(oldDom.childNodes.length, newDom.childNodes.length);
+
+        /* We should always run patch childnodes in reverse from last to first
+        because if node is removed, it removes whole element in array */
+        for (var ii = length - 1; ii >= 0; ii--) {
+          patchDomRecursively(oldDom.childNodes[ii], newDom.childNodes[ii] && newDom.childNodes[ii], oldDom);
+        }
+      }
+
+      // TODO: Extract attributes
+      // if (oldDom.nodeType === 1 && newDom.nodeType === 1) {
+      //   updateProps(oldDom, newDom.attributes, oldDom.attributes);
+      // }
+
+      if (oldDom.__radiRef) {
+        oldDom.__radiRef(oldDom);
+      }
+
+      return oldDom;
+    }
+
+    mount(newDom, parent, oldDom);
+
+    if (oldDom.__radiRef) {
+      newDom.__radiRef = oldDom.__radiRef;
+      newDom.__radiRef(newDom);
+    }
+
+    destroy(oldDom);
+  }
+
+  return newDom;
 }
 
 var Component = function Component(type) {
--- src/html/patch.js
+++ src/html/patch.js
@@ -2,10 +2,8 @@ import { destroy } from '../destroy';
 import { ensureArray } from '../utils';
 import { mount } from '../mount';
 import { render } from './render';
-// import { fireEvent } from './fireEvent';
 // import { nodeChanged } from './nodeChanged';
-// import { updateProps } from './props';
-
+import { updateProps } from './props';
 
 /**
  * @param  {Object|Object[]} nodes
@@ -59,23 +57,102 @@ export function patch(nodes, dom, parent = dom && dom.parentNode, $pointer = nul
     return outcome;
   }
 
+  /*
+    TODO: Make dom structure in before making HTMLElement
+    so that we can diff that structure with existing DOM
+
+    Preferrably it would look like this:
+
+    ```
+    {
+      type: 0,
+      query: function Component() {},
+      props: {},
+      children: [
+        {
+          type: 1,
+          query: 'button',
+          props: {
+            onclick: function dispatch() {}
+          },
+          children: [
+            {
+              type: 2,
+              query: 'Click me'
+            }
+          ]
+        }
+      ]
+    }
+    ```
+  */
+
+  /* Currently we'll render HTMLElements and diff them with real dom */
+
   const newNode = render(nodes, parent);
 
-  if (!dom && $pointer) {
-    const mounter = mount(newNode, dom, $pointer);
+  return patchDomRecursively(dom, newNode, parent, $pointer);
+}
+
+function nodeChanged(node1, node2) {
+  if (node1.nodeType === 3 && node2.nodeType === 3) return true;
+  if (node1.nodeType === node2.nodeType) return false;
+  if (node1.nodeName === node2.nodeName) return false;
+
+  return true;
+}
+
+function patchDomRecursively(oldDom, newDom, parent, pointer) {
+  if (!oldDom && (pointer || parent)) {
+    const mounter = mount(newDom, parent, pointer);
     return mounter;
   }
 
-  if (dom && parent) {
-    mount(newNode, parent, dom);
-
-    if (dom.__radiRef) {
-      newNode.__radiRef = dom.__radiRef;
-      newNode.__radiRef(newNode);
-    }
-
-    destroy(dom);
+  if (typeof newDom === 'undefined') {
+    destroy(oldDom);
+    return oldDom;
   }
 
-  return newNode;
+  if (oldDom && parent) {
+    if (!nodeChanged(oldDom, newDom)) {
+
+      /* TODO: implement text change */
+      // if (oldDom.nodeType === 3 && newDom.nodeType === 3
+      //   && oldDom.textContent !== newDom.textContent) {
+      //   oldDom.textContent = newDom.textContent;
+      // }
+
+      if (oldDom.childNodes || newDom.childNodes) {
+        const length = Math.max(oldDom.childNodes.length, newDom.childNodes.length);
+
+        /* We should always run patch childnodes in reverse from last to first
+        because if node is removed, it removes whole element in array */
+        for (let ii = length - 1; ii >= 0; ii--) {
+          patchDomRecursively(oldDom.childNodes[ii], newDom.childNodes[ii] && newDom.childNodes[ii], oldDom)
+        }
+      }
+
+      // TODO: Extract attributes
+      // if (oldDom.nodeType === 1 && newDom.nodeType === 1) {
+      //   updateProps(oldDom, newDom.attributes, oldDom.attributes);
+      // }
+
+      if (oldDom.__radiRef) {
+        oldDom.__radiRef(oldDom);
+      }
+
+      return oldDom;
+    }
+
+    mount(newDom, parent, oldDom);
+
+    if (oldDom.__radiRef) {
+      newDom.__radiRef = oldDom.__radiRef;
+      newDom.__radiRef(newDom);
+    }
+
+    destroy(oldDom);
+  }
+
+  return newDom;
 }
